1dfbb207ed9340af669043e5a1c3eab8ecdc1cad
Vega <66956728+DeusLars@users.noreply.github.com>
2024-01-23 21:30:30 +0100

    Projektdateien hinzufügen.

---
+++ Hausaufgabe701/Program.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ToDoList
+{
+    public class programm
+    {
+        static void Main()
+        {
+            TaskList taskList = new TaskList();
+            int taskCounter = 0;
+            while (true)
+            {
+                Console.WriteLine("Geben sie ein Namen ein fuer ein Task");
+                string name = Console.ReadLine();
+
+                Console.WriteLine(Environment.NewLine + "Geben sie eine Beschreibung ein");
+                string beschschreibung = Console.ReadLine();
+
+                Console.WriteLine(Environment.NewLine + "Geben sie ein Datum an");
+                string deadlineInput = Console.ReadLine();
+                DateTime deadline = Convert.ToDateTime(deadlineInput);
+
+                Task taskMachen = new Task(name, beschschreibung,  deadline);
+                taskList.Add(taskMachen);
+                taskCounter++;
+
+
+                Console.WriteLine(Environment.NewLine + "Task gespeichert");
+                //Console.WriteLine("Ausgebe: " + taskList[0].Name + " " + taskList[0].Aufgabenbeschreibung + " " + taskList[0].Deadline); //VOR ABGABE ENTFERNEN 
+                Console.WriteLine(Environment.NewLine + "Wollen sie einen Task bearbeiten, wenn ja 1 eingeben wenn nicht 0 ");
+                int bearbeitungsFrage = Convert.ToInt32(Console.ReadLine());
+                if (bearbeitungsFrage == 1)
+                {
+                    Console.WriteLine("Welchen?");
+                    int bearbeitenTask = Convert.ToInt32(Console.ReadLine());
+                    if (bearbeitenTask > 0)
+                    {
+                        bearbeitenTask--;
+                    }
+                    Console.WriteLine("Was wollen sie bearbeiten, 1 = Name, 2 = Beschreibung, 3 = Datum");
+                    int bearbeitenTaskAttribut = Convert.ToInt32(Console.ReadLine());
+
+                    if (bearbeitenTaskAttribut == 1)
+                    {
+                        taskList[bearbeitenTask].NamenAndern();
+                        taskList[bearbeitenTask].ausgabe();
+                    }
+                    if (bearbeitenTaskAttribut == 2)
+                    {
+                        taskList[bearbeitenTask].beschreibungAndern();
+                        taskList[bearbeitenTask].ausgabe();
+                    }
+                    if(bearbeitenTaskAttribut == 3)
+                    {
+                        taskList[bearbeitenTask].datumAndern();
+                        taskList[bearbeitenTask].ausgabe();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Hausaufgabe701/Task.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+
+    public class Task
+    {
+        public string Name;
+        public string Aufgabenbeschreibung;
+        public DateTime? Deadline;
+
+        
+        public Task(string name, string aufgabenbeschreibung, DateTime? deadline)
+        {
+            Name = name;
+            Aufgabenbeschreibung = aufgabenbeschreibung;
+            Deadline = deadline;
+        }
+
+        public Task(string name)
+        {
+            Name = name;
+        }
+
+        public Task(string name, string aufgabenbeschreibung)
+        {
+            Name = name;
+            Aufgabenbeschreibung = aufgabenbeschreibung;
+        }
+
+        public Task(string name, DateTime? deadline)
+        {
+            Name = name;
+            Deadline = deadline;
+        }
+
+        public void NamenAndern()
+        {
+            Console.WriteLine("Neuer Name: ");
+            Name = Console.ReadLine();
+        }
+
+        public void beschreibungAndern()
+        {
+            Console.WriteLine("Neue Beschreiubung: ");
+            Aufgabenbeschreibung = Console.ReadLine();
+        }
+        
+        public void datumAndern ()
+        {
+            Console.WriteLine("Neues Datum: ");
+            Deadline = Convert.ToDateTime(Console.ReadLine());
+        }
+
+        public void ausgabe()
+        {
+            Console.WriteLine("Das ist der Neue Eintrag: " + Name + " "+ Aufgabenbeschreibung+ " " + Deadline);
+        }
+    }
+
+    public class TaskList : List<Task>
+    {
+
+    }
+
+   
+
+
+}
